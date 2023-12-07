238007657a86b60382d26697d44a8a5561a0b078
paulgo <spam@heropaul.com>
2023-12-07 13:05:37 -0500

    --

--- App.ts
+++ App.ts
@@ -1,8 +1,8 @@
 import LogtoClient from '@logto/browser';
 
 const logtoClient = new LogtoClient({
-	appId: "rsus3iuhs9y1tgqhvz8gq",
-	endpoint: "https://ira9k5.logto.app/",
+	appId: "abc",
+	endpoint: "https://abc.logto.app/",
 });
 	
 logtoClient.signIn("http://localhost:3000/callback");
